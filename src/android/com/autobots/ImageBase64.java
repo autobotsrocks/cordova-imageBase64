ef7da514c3423b83ddc19b39aa07209ea100cb27
Tom.Huang <hzlhu.dargon@gmail.com>
2015-09-21 18:40:07 +0800

    update logic for android

--- src/android/com/autobots/ImageBase64.java
+++ src/android/com/autobots/ImageBase64.java
@@ -6,11 +6,12 @@ import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 
 import android.util.Base64;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 
 public class ImageBase64 extends CordovaPlugin {
     public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
@@ -18,14 +19,13 @@ public class ImageBase64 extends CordovaPlugin {
             final String imagePath = args.getString(0);
             cordova.getThreadPool().execute(new Runnable() {
                 public void run() {
-                    Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
-                    if (bitmap == null) {
-                        callbackContext.error("");
-                    } else {
-                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
-                        String imageBase64 = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT);
+                    try {
+                        File file = new File(imagePath);
+                        byte[] bytes = FileUtils.readFileToByteArray(file);
+                        String imageBase64 = Base64.encodeToString(bytes, 0, bytes.length, Base64.NO_WRAP);
                         callbackContext.success(imageBase64);
+                    } catch (IOException exception) {
+                        callbackContext.error(exception.getMessage());
                     }
                 }
             });
--- www/ImageBase64.js
+++ www/ImageBase64.js
@@ -1,6 +1,20 @@
 var ImageBase64 = function() {
 };
 
+ImageBase64.prototype.base64FromUrl = function(url, success, fail) {
+  var fileTransfer = new FileTransfer();
+
+  fileTransfer.download(
+    url,
+    cordova.file.cacheDirectory + '/imageBase64Cache.jpg',
+    function(entry) {
+      var imagePath = entry.toURL().replace(/^file:\/\//i, '');
+      return cordova.exec(success, fail, 'ImageBase64', 'base64', [imagePath]);
+    },
+    fail
+  );
+};
+
 ImageBase64.prototype.base64 = function(filePath, success, fail) {
   return cordova.exec(success, fail, 'ImageBase64', 'base64', [filePath]);
 };
